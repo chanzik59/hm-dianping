e5f1414d8ae7a75402c18427fc0c90034e54b364
chenzhiqin <chenzq2@allinpay.com>
2022-09-12 13:31:14 +0800

    redis 代替 session

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,6 +1,9 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
@@ -8,12 +11,16 @@ import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -30,23 +37,129 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
     @Override
     public Result queryById(Long id) {
+        Shop shopCache = getShopCacheLogicalExpire(id);
+        if (Objects.isNull(shopCache)) {
+            return Result.fail("商铺不存在");
+        }
+        return Result.ok(shopCache);
+    }
+
+    /**
+     * 利用空值和互斥锁 解决缓存穿透和击穿问题
+     *
+     * @param id
+     * @return
+     */
+    private Shop getShopCache(Long id) {
         String dataJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
         if (StrUtil.isNotBlank(dataJson)) {
-            return Result.ok(JSONUtil.toBean(dataJson, Shop.class));
+            return JSONUtil.toBean(dataJson, Shop.class);
         }
         if (dataJson != null) {
-            return Result.fail("商铺不存在");
+            return null;
         }
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        Shop shop;
+        try {
+            boolean lock = tryLock(lockKey);
+            if (!lock) {
+                Thread.sleep(50L);
+                return getShopCache(id);
+
+            }
+            shop = getById(id);
+            Thread.sleep(500);
+            if (Objects.isNull(shop)) {
+                //缓存空对象
+                stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            unLock(lockKey);
+        }
+        return shop;
+
+    }
+
+    /**
+     * 根据逻辑过期  解决缓存击穿
+     *
+     * @param id
+     * @return
+     */
+    private Shop getShopCacheLogicalExpire(Long id) {
+        String dataJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        if (StrUtil.isBlank(dataJson)) {
+            return null;
+        }
+        RedisData redisData = JSONUtil.toBean(dataJson, RedisData.class);
+        Shop shop = JSONUtil.toBean((JSONObject) redisData.getData(), Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            return shop;
+        }
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        if (tryLock(lockKey)) {
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    saveData2Redis(id, 30L);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unLock(lockKey);
+                }
+            });
+        }
+        return shop;
+
+    }
+
+
+    /**
+     * 存储逻辑过期数据
+     *
+     * @param id
+     * @param expireTime
+     */
+    public void saveData2Redis(Long id, Long expireTime) throws InterruptedException {
         Shop shop = getById(id);
-        if (Objects.isNull(shop)) {
-            //缓存空对象
-            stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("商铺不存在");
-        }
-        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return Result.ok(shop);
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireTime));
+        Thread.sleep(200);
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(redisData));
+    }
+
+
+    /**
+     * 获取互斥锁
+     *
+     * @param key
+     * @return
+     */
+    private boolean tryLock(String key) {
+        Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(aBoolean);
+    }
+
+
+    /**
+     * 删除锁
+     *
+     * @param key
+     * @return
+     */
+    private boolean unLock(String key) {
+        Boolean delete = stringRedisTemplate.delete(key);
+        return BooleanUtil.isTrue(delete);
     }
 
     @Override
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,22 @@
 package com.hmdp;
 
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    ShopServiceImpl shopService;
+
+    @Test
+    void test() throws InterruptedException {
+        shopService.saveData2Redis(1L, 10L);
+
+    }
 
 
 }
