d7cb89110f8e22d517ad7b23b7e04e0faf9c7152
chenzq <chenzq2@allinpay.com>
2022-09-28 17:07:10 +0800

    退回

--- src/main/java/com/hmdp/utils/FlashTokenInterceptor.java
+++ src/main/java/com/hmdp/utils/FlashTokenInterceptor.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class FlashTokenInterceptor implements HandlerInterceptor {
     @Autowired
@@ -42,7 +41,7 @@ public class FlashTokenInterceptor implements HandlerInterceptor {
         }
         UserDTO userDTO = BeanUtil.fillBeanWithMap(entries, new UserDTO(), false);
         UserHolder.saveUser(userDTO);
-        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY + token, RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        stringRedisTemplate.persist(RedisConstants.LOGIN_USER_KEY + token);
         return true;
     }
 
