7578e5f2d2b02e405ef4b48283049787122bc80e
chenzq <chenzq2@allinpay.com>
2022-09-29 13:28:15 +0800

    新增关注功能
    (cherry picked from commit 08b2d4e65abdb04ac2ea4d289fb103e8b1975be0)

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,13 +1,19 @@
 package com.hmdp.controller;
 
 
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 虎哥
@@ -17,4 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/follow")
 public class FollowController {
 
+    @Resource
+    private IFollowService followService;
+
+
+    /**
+     * 关注获取取消关注
+     *
+     * @param id
+     * @param follow
+     * @return
+     */
+    @PutMapping("/{id}")
+    public Result followUser(@PathVariable("id") Long id, Boolean follow) {
+        return followService.follow(id, follow);
+    }
+
+
+
+    @GetMapping("/followOrNot/{id}")
+    public Result isFollow(@PathVariable("id") Long id) {
+        return followService.isFollow(id);
+    }
 }
--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,11 +1,12 @@
 package com.hmdp.service;
 
-import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Follow;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 虎哥
@@ -13,4 +14,20 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    /**
+     * 关注用户或者取消关注用户
+     *
+     * @param id     被操作对象的用户id
+     * @param follow true 关注 false 取消关注
+     * @return
+     */
+    Result follow(Long id, Boolean follow);
+
+    /**
+     * 查询是否关注该用户
+     *
+     * @param id
+     * @return
+     */
+    Result isFollow(Long id);
 }
