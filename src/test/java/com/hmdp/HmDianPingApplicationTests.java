8b8db310ef2aa4c6ad2dd52f04fd01212ae22054
chenzhiqin <chenzq2@allinpay.com>
2022-10-13 23:25:59 +0800

    测试添加店铺坐标信息到redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,8 +1,10 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.mapper.VoucherMapper;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.service.impl.VoucherServiceImpl;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisIDMaker;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -10,14 +12,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @Slf4j
@@ -52,7 +55,17 @@ class HmDianPingApplicationTests {
         System.out.println(redisHost);
     }
 
+    /**
+     * 导入数据库店铺地理信息到redis
+     */
+    @Test
+    void loadShopGeo() {
+        Map<Long, List<Shop>> shopTypes = shopService.list().stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        Map<String, Map<String, Point>> typeGeos = shopTypes.entrySet().stream().collect(Collectors.toMap(v -> RedisConstants.SHOP_GEO_KEY + v.getKey(), v ->
+                v.getValue().stream().collect(Collectors.toMap(v1 -> String.valueOf(v1.getId()), v2 -> new Point(v2.getX(), v2.getY())))));
+        typeGeos.entrySet().stream().forEach(v -> stringRedisTemplate.opsForGeo().add(v.getKey(), v.getValue()));
 
+    }
 
 
 }
