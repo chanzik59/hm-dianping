5f77239856860e4bcd4ca5bbb5dda08b1fc1d6c9
chenzhiqin <chenzq2@allinpay.com>
2022-10-17 23:03:01 +0800

    更新redis 版本并且实现店铺距离查询
    (cherry picked from commit 7ac6cffcedafd056624a8092cef5a05adb636a7b)

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -7,13 +8,24 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
+import jodd.util.StringUtil;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -56,4 +68,39 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + shop.getId());
         return Result.ok(shop);
     }
+
+    @Override
+    public Result shopTypePage(Integer typeId, Integer current, Double x, Double y) {
+
+
+        if (x == null || y == null) {
+            // 根据类型分页查询
+            Page<Shop> page = query()
+                    .eq("type_id", typeId)
+                    .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+            // 返回数据
+            return Result.ok(page.getRecords());
+        }
+
+        String geoKey = RedisConstants.SHOP_GEO_KEY + typeId;
+        int from = (current-1)  * SystemConstants.DEFAULT_PAGE_SIZE;
+        int end = current * SystemConstants.DEFAULT_PAGE_SIZE;
+        GeoResults<RedisGeoCommands.GeoLocation<String>> results = stringRedisTemplate.opsForGeo().search(geoKey, GeoReference.fromCoordinate(x, y), new Distance(5000), RedisGeoCommands.GeoSearchCommandArgs.newGeoSearchArgs().includeDistance().limit(end));
+        if (results == null) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> content = results.getContent();
+        if (content.isEmpty() || content.size() <= from) {
+            return Result.ok(Collections.emptyList());
+        }
+        TreeMap<Long, Distance> distances = new TreeMap<>();
+        List<Long> ids = content.stream().skip(from).peek(v -> distances.put(Long.valueOf(v.getContent().getName()), v.getDistance())).map(v -> Long.valueOf(v.getContent().getName())).collect(Collectors.toList());
+
+        List<Shop> shops = query().in("id", ids).last("ORDER by FIELD(id ," + StringUtil.join(ids, ",") + ")").list();
+        shops = shops.stream().map(v -> v.setDistance(distances.get(v.getId()).getValue())).collect(Collectors.toList());
+        return Result.ok(shops);
+
+    }
+
+
 }
